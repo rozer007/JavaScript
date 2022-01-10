7715d1e4d2675c06807718fd85c4c80730e697de
rozer007 <rozerksh@gmail.com>
2022-01-10 22:02:44 +0530

    1st

---
+++ 6.Basic_DOM.js
@@ -0,0 +1,10 @@
+//window
+console.log(window)
+
+//documentx
+console.log(document)
+console.log(document.documentElement)
+
+const element = document.createElement("span")
+element.innerText="Hello World"
+document.body.appendChild(element)
